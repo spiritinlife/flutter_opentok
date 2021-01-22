83464fd03c9f710be0deddf21e44a97070d4e719
Konstantinos <kwstasarvanitakis@gmail.com>
2021-01-22 11:56:43 +0200

    registered and created the new ios factories

---
+++ ios/Classes/FlutterOpenTokSubscriberViewFactory.swift
@@ -0,0 +1,44 @@
+//
+//  FlutterOpenTokViewFactory.swift
+//  flutter_opentok
+//
+//  Created by Genert Org on 23/08/2019.
+//
+
+import Foundation
+
+public protocol FlutterSubscriberViewControllerImpl {
+    func setup()
+
+    func onMethodCall(call: FlutterMethodCall, result: @escaping FlutterResult)
+
+    func channelInvokeMethod(_ method: String, arguments: Any?)
+}
+
+class FlutterOpenTokSubscriberViewFactory: NSObject, FlutterPlatformViewFactory {
+    private let registrar: FlutterPluginRegistrar!
+
+    init(registrar: FlutterPluginRegistrar) {
+        self.registrar = registrar
+    }
+
+    func createArgsCodec() -> FlutterMessageCodec & NSObjectProtocol {
+        return FlutterStandardMessageCodec.sharedInstance()
+    }
+
+    func create(withFrame frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?) -> FlutterPlatformView {
+        // Setup logging.
+        if let arguments = args as? [String: Any],
+            let loggingEnabled = arguments["loggingEnabled"] as? Bool {
+            SwiftFlutterOpentokPlugin.loggingEnabled = loggingEnabled
+        }
+
+        let viewController: FlutterOpenTokViewController! = FlutterOpenTokViewController(frame: frame,
+                                                                                         viewIdentifier: viewId,
+                                                                                         arguments: args,
+                                                                                         registrar: registrar)
+        viewController.setup()
+
+        return viewController
+    }
+}
--- ios/Classes/SwiftFlutterOpentokPlugin.swift
+++ ios/Classes/SwiftFlutterOpentokPlugin.swift
@@ -3,10 +3,18 @@ import UIKit
 
 public class SwiftFlutterOpentokPlugin: NSObject, FlutterPlugin {
     public static var loggingEnabled: Bool = false
-
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
+        let openTokSubscriberViewFactory = FlutterOpenTokSubscriberViewFactory(registrar: registrar)
+        
+        let openTokPublisherViewFactory = FlutterOpenTokPublisherViewFactory(registrar: registrar)
+        
+        registrar.register(openTokSubscriberViewFactory as FlutterPlatformViewFactory, withId: "OPENTOK_SUBSCRIBER_VIEW")
+        
+        registrar.register(openTokPublisherViewFactory as FlutterPlatformViewFactory, withId: "OPENTOK_PUBLISHER_VIEW")
+        
         let openTokViewFactory = FlutterOpenTokViewFactory(registrar: registrar)
-
+        
         registrar.register(openTokViewFactory as FlutterPlatformViewFactory, withId: "OpenTokRendererView")
     }
 }
